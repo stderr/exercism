6d9d23e6fc78088837ddb1c4ab609e703c785c3e
Joshua Fenio <std3rr@gmail.com>
2013-08-13 01:12:51 -0600

    Add initial Clojure solution.

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,1 +1,14 @@
+(ns bob
+  (use [clojure.string :only (blank? split upper-case)]))
 
+(defn quiet? [message] (blank? message))
+(defn question? [message] (= "?" (last (split message #""))))
+(defn loud? [message] (= (upper-case message) message))
+
+
+(defn response-for [message]
+  (cond
+    (quiet? message) "Fine. Be that way."
+    (loud? message) "Woah, chill out!"
+    (question? message) "Sure."
+    :else "Whatever."))
